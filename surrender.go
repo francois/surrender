23a4e8c3d95391ce26c79e0cb8d74198c30a6c13
François Beausoleil <francois@teksol.info>
2020-09-21 10:11:26 -0400

    Simplified main loop with extract function

--- surrender.go
+++ surrender.go
@@ -51,9 +51,51 @@ func voteYearly(date time.Time, voter *VoterMemory) bool {
 	return voter.Dates[date] <= 1
 }
 
+func teaseDateFromString(text string) time.Time {
+	matches := re.FindStringSubmatch(text)
+	year, _ := strconv.ParseInt(matches[1], 10, 32)
+	monthnum, _ := strconv.ParseInt(matches[2], 10, 32)
+	day, _ := strconv.ParseInt(matches[3], 10, 32)
+
+	var month time.Month
+	switch monthnum {
+	case 1:
+		month = time.January
+	case 2:
+		month = time.February
+	case 3:
+		month = time.March
+	case 4:
+		month = time.April
+	case 5:
+		month = time.May
+	case 6:
+		month = time.June
+	case 7:
+		month = time.July
+	case 8:
+		month = time.August
+	case 9:
+		month = time.September
+	case 10:
+		month = time.October
+	case 11:
+		month = time.November
+	case 12:
+		month = time.December
+	}
+
+	return time.Date(int(year), month, int(day), 0, 0, 0, 0, location)
+}
+
+var verbose bool
+var re *regexp.Regexp
+var location *time.Location
+
 func main() {
-	re := regexp.MustCompile("\\b(19|2\\d\\d{2})[-._/]?(0[1-9]|1[012])[-._/]?(0[1-9]|[12][0-9]|3[01])")
-	location, _ := time.LoadLocation("Local")
+	verbose = true
+	re = regexp.MustCompile("\\b((?:19|[2-9]\\d)\\d{2})[-._/]?(0[1-9]|1[012])[-._/]?(0[1-9]|[12][0-9]|3[01])(?:\b|T)")
+	location, _ = time.LoadLocation("Local")
 
 	voter0 := VoterMemory{make(map[time.Time]int)}
 	voter1 := VoterMemory{make(map[time.Time]int)}
@@ -63,53 +105,23 @@ func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		matches := re.FindStringSubmatch(scanner.Text())
-		year, _ := strconv.ParseInt(matches[1], 10, 32)
-		monthnum, _ := strconv.ParseInt(matches[2], 10, 32)
-		day, _ := strconv.ParseInt(matches[3], 10, 32)
-
-		var month time.Month
-		switch monthnum {
-		case 1:
-			month = time.January
-		case 2:
-			month = time.February
-		case 3:
-			month = time.March
-		case 4:
-			month = time.April
-		case 5:
-			month = time.May
-		case 6:
-			month = time.June
-		case 7:
-			month = time.July
-		case 8:
-			month = time.August
-		case 9:
-			month = time.September
-		case 10:
-			month = time.October
-		case 11:
-			month = time.November
-		case 12:
-			month = time.December
-		}
-
-		date := time.Date(int(year), month, int(day), 0, 0, 0, 0, location)
+		date := teaseDateFromString(scanner.Text())
 
 		result0 := voteMostRecent(date, &voter0)
 		result1 := voteDaily(date, &voter1)
 		result2 := voteWeekly(date, &voter2)
 		result3 := voteMonthly(date, &voter3)
 		result4 := voteYearly(date, &voter4)
-		fmt.Fprintf(os.Stderr, "%s => recent %t, daily %t, weekly %t, monthly %t, yearly %t\n",
-			scanner.Text(),
-			result0,
-			result1,
-			result2,
-			result3,
-			result4)
+		if verbose {
+			fmt.Fprintf(os.Stderr, "%s => recent %t, daily %t, weekly %t, monthly %t, yearly %t\n",
+				scanner.Text(),
+				result0,
+				result1,
+				result2,
+				result3,
+				result4,
+			)
+		}
 		if result0 || result1 || result2 || result3 || result4 {
 			// NO OP: keep
 		} else {
