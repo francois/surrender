455ecd484a68ffac829b39d7b98410ae04d42851
François Beausoleil <francois@teksol.info>
2020-09-21 10:04:53 -0400

    Added yearly voter

--- surrender.go
+++ surrender.go
@@ -43,6 +43,14 @@ func voteMonthly(date time.Time, voter *VoterMemory) bool {
 	return voter.Dates[date] <= 1
 }
 
+// Returns true to keep, false to delete
+func voteYearly(date time.Time, voter *VoterMemory) bool {
+	date = date.AddDate(0, 0, -1*date.YearDay()+1)
+
+	voter.Dates[date] += 1
+	return voter.Dates[date] <= 1
+}
+
 func main() {
 	re := regexp.MustCompile("\\b(19|2\\d\\d{2})[-._/]?(0[1-9]|1[012])[-._/]?(0[1-9]|[12][0-9]|3[01])")
 	location, _ := time.LoadLocation("Local")
@@ -51,6 +59,7 @@ func main() {
 	voter1 := VoterMemory{make(map[time.Time]int)}
 	voter2 := VoterMemory{make(map[time.Time]int)}
 	voter3 := VoterMemory{make(map[time.Time]int)}
+	voter4 := VoterMemory{make(map[time.Time]int)}
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -93,13 +102,15 @@ func main() {
 		result1 := voteDaily(date, &voter1)
 		result2 := voteWeekly(date, &voter2)
 		result3 := voteMonthly(date, &voter3)
-		fmt.Fprintf(os.Stderr, "%s => recent %t, daily %t, weekly %t, monthly %t\n",
+		result4 := voteYearly(date, &voter4)
+		fmt.Fprintf(os.Stderr, "%s => recent %t, daily %t, weekly %t, monthly %t, yearly %t\n",
 			scanner.Text(),
 			result0,
 			result1,
 			result2,
-			result3)
-		if result0 || result1 || result2 || result3 {
+			result3,
+			result4)
+		if result0 || result1 || result2 || result3 || result4 {
 			// NO OP: keep
 		} else {
 			fmt.Println(scanner.Text())
